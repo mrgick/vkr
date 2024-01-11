86eca3e6496bb105478ba98d4f723efedfa53d19
mrgick <mrgickcool@gmail.com>
2024-01-11 21:07:43 +0300

    Backend. Update jwt. Refresh - 30 days, access - 15 min.

--- backend/src/main/urls.py
+++ backend/src/main/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from . import views
-from .jwt import CookieTokenRefreshView, CookieTokenObtainPairView
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("is-auth/", views.IsAuth.as_view(), name="is_auth"),
-    path("token/", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", CookieTokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", views.CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "token/refresh/", views.CookieTokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path("token/delete/", views.Logout.as_view(), name="token_delete"),
 ]
--- backend/src/main/views.py
+++ backend/src/main/views.py
@@ -2,6 +2,9 @@ from rest_framework_simplejwt.authentication import JWTStatelessUserAuthenticati
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.settings import api_settings
+from .serializers import CookieTokenRefreshSerializer
 
 
 class HomeView(APIView):
@@ -15,3 +18,44 @@ class IsAuth(APIView):
 
     def get(self, request):
         return Response({"user": request.user.id})
+
+
+class Logout(APIView):
+    authentication_classes = [JWTStatelessUserAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        resp = Response({"message": "success"})
+        resp.delete_cookie("refresh_token")
+        return resp
+
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get("refresh"):
+            cookie_max_age = int(api_settings.REFRESH_TOKEN_LIFETIME.total_seconds())
+            print(cookie_max_age)
+            response.set_cookie(
+                "refresh_token",
+                response.data["refresh"],
+                max_age=cookie_max_age,
+                httponly=True,
+            )
+            del response.data["refresh"]
+        return super().finalize_response(request, response, *args, **kwargs)
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get("refresh"):
+            cookie_max_age = int(api_settings.REFRESH_TOKEN_LIFETIME.total_seconds())
+            response.set_cookie(
+                "refresh_token",
+                response.data["refresh"],
+                max_age=cookie_max_age,
+                httponly=True,
+            )
+            del response.data["refresh"]
+        return super().finalize_response(request, response, *args, **kwargs)
+
+    serializer_class = CookieTokenRefreshSerializer
