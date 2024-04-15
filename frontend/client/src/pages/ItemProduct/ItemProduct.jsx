0134034f15bcbd07474c2621b1d961633db7e824
mrgick <mrgickcool@gmail.com>
2024-04-15 15:43:40 +0300

    fix

--- frontend/client/src/pages/ItemProduct/ItemProduct.jsx
+++ frontend/client/src/pages/ItemProduct/ItemProduct.jsx
@@ -168,7 +168,7 @@ const ItemProduct = (props) => {
                   d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"
                 />
               </svg>
-              <span>{object.rating}</span>
+              <span>{object.rating.toString().slice(0,4)}</span>
             </span>
           </Title>
           <section className={styles["product-top"]}>
