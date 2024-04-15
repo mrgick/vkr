6fc2a8ec10484d1754a6558142bfe9be9f988abb
mrgick <mrgickcool@gmail.com>
2024-04-15 17:27:30 +0300

    add telebot

--- backend/src/tgbot/views.py
+++ backend/src/tgbot/views.py
@@ -1,8 +1,10 @@
-import requests
 from django.conf import settings
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import telebot
+
+bot = telebot.TeleBot(settings.TELEGRAM_TOKEN)
 
 
 class BoWebHooktView(APIView):
@@ -11,20 +13,12 @@ class BoWebHooktView(APIView):
             return Response(
                 {"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED
             )
-
-        process_telegram_message(request.data)
+        print(request.data)
+        update = telebot.types.Update.de_json(request.data)
+        bot.process_new_updates([update])
         return Response({"success": True})
 
 
-def process_telegram_message(message):
-    name = message["message"]["from"]["first_name"]
-    text = message["message"]["text"]
-    chat_id = message["message"]["chat"]["id"]
-
-    reply = f"Hi {name}! Got your message: {text}"
-
-    reply_url = f"https://api.telegram.org/bot{settings.TELEGRAM_TOKEN}/sendMessage"
-
-    data = {"chat_id": chat_id, "text": reply}
-
-    requests.post(reply_url, data=data)
+@bot.message_handler(commands=["start"])
+def greet(m):
+    bot.send_message(m.chat.id, "Hello")
