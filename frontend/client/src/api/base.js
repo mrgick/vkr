101766ee58a339f32af5fda2c6fc456add85d201
mrgick <mrgickcool@gmail.com>
2024-04-18 16:19:03 +0300

    Change api.

--- frontend/client/src/api/base.js
+++ frontend/client/src/api/base.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "https://dice-harmony.onrender.com/";
 
 export const resolve = async (promise) => {
   const resolved = {
