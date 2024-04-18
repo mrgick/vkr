8e0b95a78812d24a6acf38890e744ae90b099cee
mrgick <mrgickcool@gmail.com>
2024-04-18 20:48:05 +0300

    Update telegram bot.

--- backend/src/tgbot/views.py
+++ backend/src/tgbot/views.py
@@ -6,6 +6,8 @@ import telebot
 from telebot import types
 from telebot.util import quick_markup
 from shop.models import Category, Product
+from news.models import News
+from django.db.models import Q
 
 import json
 
@@ -18,7 +20,6 @@ class BoWebHooktView(APIView):
             return Response(
                 {"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED
             )
-        # print(request.data)
         update = types.Update.de_json(request.data)
         bot.process_new_updates([update])
         return Response({"success": True})
@@ -30,6 +31,30 @@ def greet(message):
         message.chat.id,
         "Привет! Я - бот Dice Harmony, ваш помощник в мире настольных игр. Я могу рассказать вам о наших новинках, помочь найти игру в нашем магазине. Чем я могу вам помочь?",
     )
+
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(
+        message.chat.id,
+        "Список доступных команд:\n\n/help - справка\n\n/categories - список категорий\n/news - список новостей\n\n/site - адрес сайта\n/contacts - контакты\n\nКроме того, есть быстрый поиск по имени товара/новости",
+    )
+
+
+@bot.message_handler(commands=["site"])
+def site(message):
+    markup = quick_markup({"Открыть сайт": {"url": "https://dice-harmony.ru"}})
+    bot.send_message(message.chat.id, "https://dice-harmony.ru", reply_markup=markup)
+
+
+@bot.message_handler(commands=["contacts"])
+def contacts(message):
+    bot.send_message(
+        message.chat.id,
+        "Контакты:\n\nEmail - dice-harmony@yandex.ru\nТелефон - +7 (000) 000-0000\nРазработчик - [mrgick](https://github.com/mrgick)\nАдрес - улица Труда, 54, Псков, 180019\n",
+        parse_mode="markdown",
+    )
+    bot.send_location(message.chat.id, 57.840474, 28.350750)
 
 
 @bot.message_handler(commands=["categories"])
@@ -50,6 +75,24 @@ def categories(message):
     )
 
 
+@bot.message_handler(commands=["news"])
+def news(message):
+    news = News.objects.all()[0]
+    max_page = News.objects.count()
+    markup = pagination(
+        1,
+        max_page,
+        {"news": True},
+        {"text": "Подробнее", "url": f"{settings.NEWS_LINK}/{news.id}"},
+    )
+    bot.send_message(
+        message.chat.id,
+        f"*{news.title}*\n\n{news.description}\n[\u200B]({news.image.url})",
+        reply_markup=markup,
+        parse_mode="markdown",
+    )
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def handle_button_click(call):
     res = None
@@ -57,31 +100,54 @@ def handle_button_click(call):
         data = json.loads(call.data)
     except Exception:
         bot.send_message(call.message.chat.id, "Команда не распознана(")
+        return
+    if "hide" in data:
+        bot.edit_message_reply_markup(
+            call.message.chat.id,
+            call.message.id,
+        )
+    elif "category" in data:
+        category = data["category"]
+        page = data.get("page", 1)
+        product = Product.objects.filter(category=category, stock=True).all()[
+            page - 1 : page
+        ][0]
+        max_page = Product.objects.filter(category=category, stock=True).count()
+        markup = pagination(
+            page,
+            max_page,
+            {"category": category},
+            {"text": "Купить", "url": f"{settings.PRODUCT_LINK}/{product.id}"},
+        )
+        bot.edit_message_text(
+            f"*★{str(product.rating)[:3]}  -  {product.title}  -  {product.price}₽*\n\n{product.description}\n[\u200B]({product.image.url})",
+            call.message.chat.id,
+            call.message.id,
+            reply_markup=markup,
+            parse_mode="markdown",
+        )
+    elif "news" in data:
+        page = data.get("page", 1)
+        news = News.objects.all()[page - 1 : page][0]
+        max_page = News.objects.count()
+        markup = pagination(
+            page,
+            max_page,
+            {"news": True},
+            {"text": "Подробнее", "url": f"{settings.NEWS_LINK}/{news.id}"},
+        )
+        bot.edit_message_text(
+            f"{news.date.date()}  -  *{news.title}*\n\n{news.description}\n[\u200B]({news.image.url})",
+            call.message.chat.id,
+            call.message.id,
+            reply_markup=markup,
+            parse_mode="markdown",
+        )
     else:
-        if "hide" in data:
-            res = bot.edit_message_reply_markup(
-                call.message.chat.id,
-                call.message.id,
-            )
-        elif "category" in data:
-            category = data["category"]
-            page = data.get("page", 1)
-            product = Product.objects.filter(category=category, stock=True).all()[page - 1 : page][
-                0
-            ]
-            max_page = Product.objects.filter(category=category, stock=True).count()
-            markup = pagination(page, max_page, {"category": category})
-            # print(product.image.url)
-            res = bot.edit_message_text(
-                f"*{product.title} ★{str(product.rating)[:3]} {product.price}₽*\n\n{product.description}\n[\u200B]({product.image.url})",
-                call.message.chat.id,
-                call.message.id,
-                reply_markup=markup,
-                parse_mode="markdown",
-            )
+        bot.send_message(call.message.chat.id, "Команда не распознана(")
 
 
-def pagination(page: int, max_page: int, data: dict):
+def pagination(page: int, max_page: int, data: dict, btn_url: dict = None):
 
     buttons = []
     if page > 1:
@@ -103,17 +169,55 @@ def pagination(page: int, max_page: int, data: dict):
                 text=">", callback_data=json.dumps({"page": page + 1, **data})
             )
         )
-    buttons.append(
+    markup = types.InlineKeyboardMarkup()
+    markup.row(*buttons)
+    if btn_url:
+        markup.row(types.InlineKeyboardButton(text=btn_url["text"], url=btn_url["url"]))
+    markup.row(
         types.InlineKeyboardButton(
             text="Скрыть", callback_data=json.dumps({"hide": True, **data})
         )
     )
-    markup = types.InlineKeyboardMarkup(row_width=len(buttons) - 1)
-    markup.add(*buttons)
+
     return markup
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    print(2)
-    bot.reply_to(message, message.text)
+    if len(Category.objects.filter(title__icontains=message.text)) > 0:
+        categories(message)
+    elif (
+        product := Product.objects.filter(title__icontains=message.text).first()
+    ) is not None:
+        markup = types.InlineKeyboardMarkup()
+        markup.row(
+            types.InlineKeyboardButton(
+                text="Подробнее", url=f"{settings.PRODUCT_LINK}/{product.id}"
+            )
+        )
+        bot.send_message(
+            message.chat.id,
+            f"*★{str(product.rating)[:3]}  -  {product.title}  -  {product.price}₽*\n\n{product.description}\n[\u200B]({product.image.url})",
+            reply_markup=markup,
+            parse_mode="markdown",
+        )
+    elif (
+        news := News.objects.filter(title__icontains=message.text).first()
+    ) is not None:
+        markup = types.InlineKeyboardMarkup()
+        markup.row(
+            types.InlineKeyboardButton(
+                text="Подробнее", url=f"{settings.NEWS_LINK}/{news.id}"
+            )
+        )
+        bot.send_message(
+            message.chat.id,
+            f"{news.date.date()}  -  *{news.title}*\n\n{news.description}\n[\u200B]({news.image.url})",
+            reply_markup=markup,
+            parse_mode="markdown",
+        )
+    else:
+        bot.send_message(
+            message.chat.id,
+            "Поиск по сообщению ничего не дал(\nСписок доступных команд: /help",
+        )
